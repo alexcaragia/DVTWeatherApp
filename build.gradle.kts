a1ff38d12ad562be5f6a6cb0580b5978cac7851f
Alex Caragia <allexandrucaragia@gmail.com>
2022-10-17 17:12:41 +0300

    Migrated to Gradle Kotlin DSL & Version Catalog

---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+buildscript {
+    dependencies {
+        classpath(libs.kotlin.gradle.plugin)
+        classpath(libs.androidx.safeargs.gradle.plugin)
+    }
+}
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.android) apply false
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
